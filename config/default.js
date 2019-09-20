74b3a591e99c6d76e5c35ec89d1169ae078a602f
CWD <cwd@cloudspokes.com>
2019-09-20 19:33:25 +0000

    config

---
+++ config/default.js
@@ -0,0 +1,6 @@
+module.exports = {
+  SLACK_CLIENTID: process.env.SLACK_CLIENTID || '',
+  SLACK_REDIRECTURL: process.env.SLACK_REDIRECTURL || '',
+  SLACK_SCOPE: process.env.SLACK_SCOPE || '',
+  SLACK_TOKEN: process.env.SLACK_TOKEN | ''
+}
