14b13d7de84af59fb6852fe6034264718540048f
CWD <cwd@cloudspokes.com>
2019-09-20 19:42:45 +0000

    fixing up config

--- config/default.js
+++ config/default.js
@@ -1,6 +1,7 @@
 module.exports = {
   SLACK_CLIENTID: process.env.SLACK_CLIENTID || '',
+  SLACK_CLIENTSECRET: process.env.SLACK_CLIENTSECRET || 'f' ,
   SLACK_REDIRECTURL: process.env.SLACK_REDIRECTURL || '',
-  SLACK_SCOPE: process.env.SLACK_SCOPE || '',
-  SLACK_TOKEN: process.env.SLACK_TOKEN | ''
+  SLACK_SCOPE: process.env.SLACK_SCOPE || 'chat:write:bot,chat:write:user,im:history',
+  SLACK_TOKEN: process.env.SLACK_TOKEN || ''
 }
