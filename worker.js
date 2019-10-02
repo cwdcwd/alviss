196336faead7a830c7ab832d47bb86c59fc650da
CWD <cwd@cloudspokes.com>
2019-10-02 18:56:19 +0000

    snagged example worker for use w/ Azure service bus

---
+++ worker.js
@@ -0,0 +1,36 @@
+console.log('worker goes here');
+
+const _ = require('lodash');
+const config = require('config');
+const { ServiceBusClient } = require("@azure/service-bus");
+
+// Define connection string and related Service Bus entity names here
+const queueName = "";
+
+async function main() {
+  const sbClient = ServiceBusClient.createFromConnectionString(config.AZURE_SERVICE_BUS);
+  const queueClient = sbClient.createQueueClient(config.AZURE_QUEUE);
+  const sender = queueClient.createSender();
+
+  try {
+    for (let i = 0; i < 10; i++) {
+      const message = {
+        body: `Hello world! ${i}`,
+        label: `test`,
+        userProperties: {
+          myCustomPropertyName: `my custom property value ${i}`
+        }
+      };
+      console.log(`Sending message: ${message.body}`);
+      await sender.send(message);
+    }
+
+    await queueClient.close();
+  } finally {
+    await sbClient.close();
+  }
+}
+
+main().catch((err) => {
+  console.log("Error occurred: ", err);
+});
